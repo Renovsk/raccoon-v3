cc2786a42f2db279145bbf867d1f488417da52e5
crxelty <104393390+Renovsk@users.noreply.github.com>
2025-03-04 11:21:21 -0500

    made better
    added raw.txt saves
    made it not save PROTECTED ips
    thats it fr

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 import requests
 import threading
 import os
-
+import time
 
 w = '\033[38;2;255;255;255m'
 c = '\033[38;2;245;174;187m'
@@ -10,8 +10,8 @@ c = '\033[38;2;245;174;187m'
 url = "https://minecraft-mp.com/servers/random/"
 
 def clean():
-    print(f'{c}[{w}*{c}]{w} Cleaning ips.txt')
-    with open('ips.txt', 'w') as i :
+    print(f'{c}[{w}*{c}]{w} Cleaning ips.txt & raw.txt')
+    with open('ips.txt', 'w') as i:
         i.write(fr'''
  __         ______     ______     ______    
 /\ \       /\  __ \   /\  ___\   /\  ___\   
@@ -21,6 +21,9 @@ def clean():
        Raccoon Logs | Made by crxelty''')
         print(f'{c}[{w}+{c}]{w} Cleaned ips.txt successfully!')
         print(f'-')
+    with open('raw.txt', 'w') as z:
+        z.write('')
+        print(f'{c}[{w}+{c}]{w} Cleaned raw.txt successfully!')
 
 def check_protection(server):
     res = requests.get(f'https://networkcalc.com/api/dns/lookup/{server}').text.lower()
@@ -35,23 +38,36 @@ def main():
         servers = re.findall(r'<strong>(.*?)</strong></button>', r.text)
         for ip in servers:
             try:
-                with open('ips.txt', 'a+') as i:
-                    server = requests.get(f'https://api.mcstatus.io/v2/status/java/{ip}').json()
-                    raw = server['ip_address']
-                    players = f'{server['players']['online']}/{server['players']['max']}'
-                    protected, protype = check_protection(ip)
-                    if server['players']['online'] and raw not in i.read():
-                        print(f'{c}[{w}+{c}] {w}Found Server! │ {c}IP:{w} {str(raw).ljust(15)} {w}│ {c}Players:{w} {str(players).ljust(9)} {w}│ {c}Protected?:{w} {'True ' if protected == True else 'False'} ({protype if protype != '' else raw})')
-                        i.write(f'\n=============================================\nDomain: {ip}\nIP: {raw} | Players: {players} | {c}Protected?: {protected} ({protype if protype != '' else raw})')
+                server = requests.get(f'https://api.mcstatus.io/v2/status/java/{ip}').json()
+                raw = server['ip_address']
+                players = f"{server['players']['online']}/{server['players']['max']}"
+                ver = server['version']['name_raw']
+                protected, protype = check_protection(ip)
 
-            except: pass
+                if server['players']['online'] and raw not in open('ips.txt').read() and 'velocity' not in ver.lower():
+                    info = (
+                        f"{c}[{w}+{c}] {w}Found Server! │ "
+                        f"{c}IP:{w} {str(raw).ljust(15)} │ "
+                        f"{c}Players:{w} {str(players[:13]).ljust(13)} │ "
+                        f"{c}Protected?:{w} {str(protected).ljust(5)} ({str(protype + ')' if protype != '' else raw + ')').ljust(17)} │ "
+                        f"{c}Version: {w}({ver[:20]})"
+                    )
+                    print(info)
+                    if not protected:
+                        with open('ips.txt', 'a+', encoding='utf-8') as i: 
+                            i.write(f'\n─────────────────────────────────────────\nDomain: {ip}\nIP: {raw}\nPlayers: {players}\nProtected?: {protected} ({protype if protype != "" else raw})')
+                        with open('raw.txt', 'a+', encoding='utf-8') as gg: 
+                            gg.write(f'{raw}\n')
+            
+            except Exception as e:
+                pass
 
 if __name__ == '__main__':
     clean()
     os.system('cls')
     print(f'{w}raccoon scanner {c}v3')
     threads = []
-    for i in range(30):
+    for i in range(50):
         t = threading.Thread(target=main)
         t.start()
         threads.append(t)
